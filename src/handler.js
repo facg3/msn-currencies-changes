96270d57b3b9410699759b9745be2f3a436203b6
MahmoudMH <m0597731363@gmail.com>
2017-12-06 09:24:15 +0200

    genric method

--- src/handler.js
+++ src/handler.js
@@ -12,5 +12,29 @@ const handleHomePage = (request, response) => {
     }
   });
 };
+const generic = (request, response) => {
+  const endpoint = request.url;
+  const extension = endpoint.split('.')[1];
+  const fileType = {
+    html: 'text/html',
+    css: 'text/css',
+    js: 'application/javascript',
+    ico: 'image/x-icon',
+    json: 'application/json',
+  };
+  fs.readFile(path.join(__dirname, '..', endpoint), (error, file) => {
+    if (error) {
+      response.writeHead(500, 'content-Type:text/html');
+      response.end('<h1> Internal server Error </h1>');
+    } else {
+      response.writeHead(200, `content-Type:${fileType[extension]}`);
+      response.end(file);
+    }
+  });
+};
 
-module.exports = { handleHomePage };
+
+module.exports = {
+  handleHomePage,
+  generic,
+};
--- src/tests/routs.test.js
+++ src/tests/routs.test.js
@@ -2,10 +2,15 @@ const shot = require('shot');
 const test = require('tape');
 const router = require('../router');
 
-console.log('test pass plaplaplaplaa');
 test('handle Home Page', (t) => {
   shot.inject(router, { method: 'get', url: '/' }, (res) => {
     t.equal(res.statusCode, 200, 'should respond with status code of 200');
+    t.end();
+  });
+});
+test('handle one of generic pages', (t) => {
+  shot.inject(router, { method: 'get', url: '/public' }, (response) => {
+    t.equal(response.statusCode, 500, 'success handle page Internal server error');
     t.end();
   });
 });
